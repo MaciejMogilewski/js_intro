f341a8ccd077bdc3213ed6519000abb3f80ca672
Maciej <maciej.mogilewski@gmail.com>
2022-08-06 10:59:16 +0200

    variables with examples

---
+++ src/variables.js
@@ -0,0 +1,60 @@
+// Zmienne
+
+// var x = 42;
+// let y = 42;
+// const z = 42;
+
+// console.log(x, y, z);
+
+// Redeklaracja
+// var x = 666; // ok
+// let y =666; // no
+// const z = 666; // no
+// console.log(x, y, z);
+
+// Reinicjalizacja, ponowne przypisanie wartości
+// x = 666; // ok
+// y = 666; // ok
+// z = 666; // no
+// console.log(x, y, z);
+// Hoisting - przenoszenie deklaracji zmiennych var, deklaracji funkcji i klas  na góre aktualnie przetwarzangego pilku
+
+// console.log(magic);
+// var magic = 42;
+//
+// var magic2;
+// console.log(magic2;
+// magic2 = 42;
+
+// console.log(doMagic());
+//
+// function doMagic() {
+//     return 42;
+// }
+
+// Zasięg - scope - funkcyjny, blokowy
+// blok - zesatw dwóch klamerek {}
+// var - funkcyjny
+// var x = 42;
+//
+// if (true) {
+//     var x = 666;
+//     console.log(x);
+// }
+//
+// console.log(x);
+
+// let i const zasięg blokowy
+//
+
+// RHS (odczyt) i LHS (zapis) - right hand side assigment and left...
+
+
+// let x = 42;
+//
+// if (true) {
+//     x = 666;
+//     console.log(x);
+// }
+//
+// console.log(x);
